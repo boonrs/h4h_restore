474daa45264911c19300b44e137e64aab21cf5c4
Chris Thorn <thorncp@gmail.com>
2013-04-21 10:16:24 -0800

    Seed test user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+unless Rails.env.production?
+  User.find_or_create_by_email "admin@example.com" do |user|
+    user.password = "sellingstuff"
+    user.password_confirmation = "sellingstuff"
+  end
+end
